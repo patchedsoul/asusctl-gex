64f3e53ba1bfb15ce34957d576863a46e143f553
DualityKyle <1455995-DualityKyle@users.noreply.gitlab.com>
2022-01-25 00:03:50 -0400

    Added display logic for supergfxctl version
    Only show graphics switching if supergfxctl version is supported

--- src/modules/gfx_mode.ts
+++ src/modules/gfx_mode.ts
@@ -4,7 +4,7 @@ declare var ext: any;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 const {main, popupMenu} = imports.ui;
-const {Gio, GLib} = imports.gi;
+const {Gio, GLib, St} = imports.gi;
 
 import * as Log from './log';
 import * as DBus from './gfx_mode_dbus';
@@ -15,6 +15,7 @@ export class Client implements IStoppableModule, IPopulatePopupModule {
     iGpuString: string = 'unknown';
     connector: DBus.GfxMode = new DBus.GfxMode();
     connected: boolean = false;
+    updateGfxItem: any;
 
     constructor() {
         Log.debug(`Starting GfxMode client...`);
@@ -57,9 +58,6 @@ export class Client implements IStoppableModule, IPopulatePopupModule {
     populatePopup(): void {
         if (this.isRunning() && this.connector.supported.length > 0){
 
-            // add the GPU icon to the panel icon bin
-            ext.panelButton._indicatorLayout.add_child(ext.panelButton._binGpu);
-
             let vendor = this.connector.getGfxMode();
             let gpuPower = this.connector.getGpuPower();
 
@@ -69,72 +67,100 @@ export class Client implements IStoppableModule, IPopulatePopupModule {
 
             menu.addMenuItem(new popupMenu.PopupMenuItem('Graphics Mode', {hover: false, can_focus: false, style_class: 'headline gfx headline-label asusctl-gex-menu-item'}), 0);
 
-            Log.debug(`Current Graphics Mode is ${this.connector.gfxLabels[vendor]}`);
+            // Graphics switching options and the indicator icon in the panel
+            // should only be shown if the install version of supergfxctl is 
+            // supported by asusctl-gex
+            if (this. connector.getVersion() == this.connector.supergfxSupportedVer) {
+                
+                // add the GPU icon to the panel icon bin
+                ext.panelButton._indicatorLayout.add_child(ext.panelButton._binGpu);
+                
+                Log.debug(`Current Graphics Mode is ${this.connector.gfxLabels[vendor]}`);
             
-            if (typeof gpuPower !== 'undefined') {
-                let gpuPowerItem = new popupMenu.PopupImageMenuItem(
-                    `dedicated GPU: ${this.connector.gfxLabels[gpuPower]}`,
-                    Gio.icon_new_for_string(`${Me.path}/icons/scalable/dgpu-${this.connector.gfxLabels[gpuPower]}.svg`),
-                {
-                    hover: false,
-                    can_focus: false,
-                    style_class: `gpupower ${this.connector.gfxLabels[gpuPower]} asusctl-gex-menu-item`
-                }
-                );
+                if (typeof gpuPower !== 'undefined') {
+                    let gpuPowerItem = new popupMenu.PopupImageMenuItem(
+                        `dedicated GPU: ${this.connector.gfxLabels[gpuPower]}`,
+                        Gio.icon_new_for_string(`${Me.path}/icons/scalable/dgpu-${this.connector.gfxLabels[gpuPower]}.svg`),
+                    {
+                        hover: false,
+                        can_focus: false,
+                        style_class: `gpupower ${this.connector.gfxLabels[gpuPower]} asusctl-gex-menu-item`
+                    }
+                    );
 
-                menu.addMenuItem(gpuPowerItem, menuIdx++);
+                    menu.addMenuItem(gpuPowerItem, menuIdx++);
 
-                // seperator
-                menu.addMenuItem(new popupMenu.PopupSeparatorMenuItem(), menuIdx++);
-            }
-
-            let indexIntegrated = 0;
-            this.connector.supported.forEach((supported: number) => {
-                menuIdx++;
-
-                let localIndex = menuIdx;
-
-                let labelMenu = this.connector.gfxLabels[supported];
-                if (labelMenu == 'integrated'){
-                    indexIntegrated = localIndex;
+                    // seperator
+                    menu.addMenuItem(new popupMenu.PopupSeparatorMenuItem(), menuIdx++);
                 }
 
-                if (labelMenu == 'vfio' || labelMenu == 'compute'){
-                    labelMenu = '↳ '+labelMenu;
-                    localIndex = indexIntegrated;
-                }
+                let indexIntegrated = 0;
+                this.connector.supported.forEach((supported: number) => {
+                    menuIdx++;
 
-                let menuItem = new popupMenu.PopupImageMenuItem(
-                    labelMenu,
-                    Gio.icon_new_for_string(`${Me.path}/icons/scalable/gpu-${this.connector.gfxLabels[supported]}.svg`),
-                {style_class: `${this.connector.gfxLabels[supported]} gfx-mode ${this.iGpuString} asusctl-gex-menu-item`}
-                );
+                    let localIndex = menuIdx;
 
-                let idx = this.connector.gfxLabels.indexOf(this.connector.gfxLabels[supported]);
-                let acl = this.getAcl(vendor, idx);
+                    let labelMenu = this.connector.gfxLabels[supported];
+                    if (labelMenu == 'integrated'){
+                        indexIntegrated = localIndex;
+                    }
 
-                // set active item
-                if (idx === vendor) {
-                    menuItem.style_class = `${menuItem.style_class} active asusctl-gex-menu-item`;
-                    menuItem.label.set_text(`${menuItem.label.text}  ✔`);
-                }
+                    if (labelMenu == 'vfio' || labelMenu == 'compute'){
+                        labelMenu = '↳ '+labelMenu;
+                        localIndex = indexIntegrated;
+                    }
 
-                // add to menu
+                    let menuItem = new popupMenu.PopupImageMenuItem(
+                        labelMenu,
+                        Gio.icon_new_for_string(`${Me.path}/icons/scalable/gpu-${this.connector.gfxLabels[supported]}.svg`),
+                    {style_class: `${this.connector.gfxLabels[supported]} gfx-mode ${this.iGpuString} asusctl-gex-menu-item`}
+                    );
 
-                menu.addMenuItem(menuItem, localIndex);
+                    let idx = this.connector.gfxLabels.indexOf(this.connector.gfxLabels[supported]);
+                    let acl = this.getAcl(vendor, idx);
 
-                // check and set acl (true == access granted)
-                menuItem.sensitive = acl;
-                menuItem.active = acl;
-                menuItem.connect('activate', () => {
-                    // delay poller, only on integrated(1) and swithing to vfio(3)
-                    if (this.connector.lastState == 1 && idx == 3)
-                        this.connector.pollerDelayTicks = 5;
-                    this.connector.setGfxMode(idx);
+                    // set active item
+                    if (idx === vendor) {
+                        menuItem.style_class = `${menuItem.style_class} active asusctl-gex-menu-item`;
+                        menuItem.label.set_text(`${menuItem.label.text}  ✔`);
+                    }
+
+                    // add to menu
+
+                    menu.addMenuItem(menuItem, localIndex);
+
+                    // check and set acl (true == access granted)
+                    menuItem.sensitive = acl;
+                    menuItem.active = acl;
+                    menuItem.connect('activate', () => {
+                        // delay poller, only on integrated(1) and swithing to vfio(3)
+                        if (this.connector.lastState == 1 && idx == 3)
+                            this.connector.pollerDelayTicks = 5;
+                        this.connector.setGfxMode(idx);
+                    });
                 });
-            });
 
-            menu.addMenuItem(new popupMenu.PopupSeparatorMenuItem(), menuIdx++);
+                menu.addMenuItem(new popupMenu.PopupSeparatorMenuItem(), menuIdx++);
+            } else {
+                // If installed version of supergfxctl isn't supported, don't show 
+                // graphics menu options. Instead, show a warning to upgrade.
+                
+                // Parent Menu item
+                this.updateGfxItem = new popupMenu.PopupBaseMenuItem({style_class: 'asusctl-gex-menu-item'});
+
+                // Create icon and label for the warning. We use this longer method so 
+                // that we can make only the white icon orange for greater visibility :)
+                let textUpdateGfx = 'Please update supergfxctl to version\n' + this.connector.supergfxSupportedVer + ' to enable graphics support!'
+                let iconUpdateGfx = new St.Icon({icon_name: 'software-update-available-symbolic', icon_size: 28, style_class: 'popup-menu-icon popup-menu-icon-orange'});
+                let labelUpdateGfx = new St.Label({text: textUpdateGfx, style_class: 'asusctl-gex-menu-item'});
+
+                // Add the above two sub-tems to the parent menu item
+                this.updateGfxItem.add(iconUpdateGfx);
+                this.updateGfxItem.add(labelUpdateGfx);
+
+                // Add menu item to main panel menu
+                menu.addMenuItem(this.updateGfxItem, menuIdx++);
+            }
         }
     }
 }
